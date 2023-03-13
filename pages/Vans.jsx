676a089d8a995121dfe5fa4e8643a493d0c5fd8c
puepi <pmandel0001@yahoo.fr>
2023-03-13 13:24:31 +0100

    changed the vans component page

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,7 +1,16 @@
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
+// import {useState,useEffect} from 'react'
+import React from 'react'
 
 export default function Vans(){
+    // const [vansList,setVansList]=useState([])
+    React.useEffect(() => {
+        fetch("/api/vans")
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err=>console.log(err))
+    }, [])
     return(
-        <h1>Vans page goes here 🚐</h1>
+        <h1>Vans page goes here! 🚐</h1>
     )
 }
