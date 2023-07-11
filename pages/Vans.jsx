ac53fb49041c82d730ebdf4565535b2b73716a1d
Mandela Puepi <pmandel0001@yahoo.fr>
2023-07-11 20:50:42 +0100

    added vanslist page

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,16 +1,36 @@
 // import {Link} from 'react-router-dom'
 // import {useState,useEffect} from 'react'
-import React from 'react'
+import React,{useState} from 'react'
 
 export default function Vans(){
-    // const [vansList,setVansList]=useState([])
+    const [vansList,setVansList]=useState([])
     React.useEffect(() => {
         fetch("/api/vans")
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => setVansList(data.vans))
             .catch(err=>console.log(err))
     }, [])
+
+    const vansListElements=vansList.map(van=>{
+        return(
+            <div key={van.id} className='van-tile'>
+                <img src={van.imageUrl} />
+                <div className='van-info'>
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </div>
+        )
+    })
+
     return(
-        <h1>Vans page goes here! 🚐</h1>
+        <React.Fragment>
+            <div className='van-list-container'>
+                <div className='van-list'>
+                    {vansListElements}
+                </div>
+            </div>
+        </React.Fragment>
     )
 }
